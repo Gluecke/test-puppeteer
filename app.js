b2d554f3a97e88fb14e5e2b13c8f277d991d99d6
Garrett Luecke <garretteluecke@gmail.com>
2020-01-16 22:41:18 -0600

    initial comit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  console.log("started");
+  const pngName = "goog-home.png";
+
+  let fp = (async () => {
+    const browser = await puppeteer.launch({
+      defaultViewport: null,
+      args: ["--window-size=1920,1080"]
+    });
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+    await page.goto("https://google.com");
+
+    console.log("saying hi");
+    await page.type('input', 'hello');
+
+    console.log("snapping a pic");
+    await page.screenshot({ path: pngName });
+
+    await page.close();
+    await browser.close();
+  })();
+
+  await fp;
+
+  res.set("Content-Type", "image/png");
+
+  s = fs.createReadStream("./" + pngName);
+
+  s.pipe(res);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Puppeteer test app
+
+test app for running puppeteer, taking a screenshot and returning it
+
+### Installing
+
+installing puppeteer will install a compatible version of chromium
+
+```
+npm install
+```
+
+## Running
+
+```
+node app.js
+```
+
+navigate to `http://localhost:3000/`
